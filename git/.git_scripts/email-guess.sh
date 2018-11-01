0cd531504e47a82e68b19ce93005516d371c57c1
David Warden <dfwarden@gmail.com>
2018-11-01 11:37:12 -0400

    default to geneseo gpg signing key when checking out a geneseo repo

--- git/.git_scripts/email-guess.sh
+++ git/.git_scripts/email-guess.sh
@@ -4,7 +4,9 @@ remote=`git remote -v | awk '/\(push\)$/ {print $2}'`
 email=dfwarden@gmail.com # default
 
 if [[ $remote == *geneseo.edu* ]]; then
-      email=warden@geneseo.edu
+    email=warden@geneseo.edu
+    # Use Geneseo signing key
+    git config user.signingKey 0x4B89C54E5455EA50
 fi
 
 echo "Configuring user.email as $email"
