bcf74c15c6b7c01a85ee60dffca28691e9c7e969
David Warden <dfwarden@gmail.com>
2019-01-01 17:18:11 -0500

    starting to reorganize such that install.sh is more platform-independent

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,13 +8,11 @@
 # Default to show you everything the script is doing.
 set -x
 
-STOW_PACKAGES=(git vim zsh)
+# Things to do in this dotfiles repo
 
 # Get the base path of where this repo was cloned to and go there
 REPO="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )" )" >/dev/null && pwd)"
 cd $REPO
-
-brew bundle
 
 # unlock git-crypt dotfiles, requires git-crypt and gnupg
 git-crypt unlock
@@ -23,30 +21,21 @@ git-crypt unlock
 # xml for diff purposes.
 git config diff.plist.textconv 'git plutil-toxml1'
 
-for stow_package in "${STOW_PACKAGES[@]}"; do
-  stow $stow_package
+# GNU Stow
+STOW_PACKAGES_HOME=(git vim zsh)
+STOW_PACKAGES_LOCALBIN=(bin)
+
+for stow_package in "${STOW_PACKAGES_HOME[@]}"; do
+  stow -t ${HOME} $stow_package
+done
+for stow_package in "${STOW_PACKAGES_LOCALBIN[@]}"; do
+  stow -t /usr/local/bin $stow_package
 done
 
-# make sure zsh is current shell
-CURRENT_SHELL=$(dscl . -read /Users/${USER} UserShell | awk '{print $2}')
-if [ "$CURRENT_SHELL" != "/bin/zsh" ]; then
-    chsh -s $(which zsh) ${USER}
-fi
+# ZSH themes and plugins
 cp "zsh/themes/"*".zsh-theme" "${HOME}/.oh-my-zsh/custom/themes"
 git clone https://github.com/zsh-users/zsh-completions "${HOME}/.oh-my-zsh/custom/plugins/zsh-completions"
 
 # Install Vundle plugins from .vimrc
 vim +PluginUpgrade +qall
 
-# Better Touch Tool
-BTT="${HOME}/Library/Application Support/BetterTouchTool"
-mkdir -p $BTT
-stow bettertouchtool -t $BTT
-stow library-preferences -t $HOME/Library/Preferences
-
-# Set up LoginItems
-loginitems -a BetterTouchTool
-loginitems -a MenuMetersApp -p "${HOME}/Library/PreferencePanes/MenuMeters.prefPane/Contents/Resources/MenuMetersApp.app"
-loginitems -a 'Alfred 3'
-loginitems -a Dash
-loginitems -a Caffeine
--- scripts/install_mac.sh
+++ scripts/install_mac.sh
@@ -3,6 +3,28 @@
 # Default to show you everything the script is doing.
 set -x
 
+brew bundle
+
+# make sure zsh is current shell
+CURRENT_SHELL=$(dscl . -read /Users/${USER} UserShell | awk '{print $2}')
+if [ "$CURRENT_SHELL" != "/bin/zsh" ]; then
+    chsh -s $(which zsh) ${USER}
+fi
+
+# Better Touch Tool
+BTT="${HOME}/Library/Application Support/BetterTouchTool"
+mkdir -p $BTT
+stow bettertouchtool -t $BTT
+stow library-preferences -t $HOME/Library/Preferences
+
+# Set up LoginItems
+loginitems -a BetterTouchTool
+loginitems -a MenuMetersApp -p "${HOME}/Library/PreferencePanes/MenuMeters.prefPane/Contents/Resources/MenuMetersApp.app"
+loginitems -a 'Alfred 3'
+loginitems -a Dash
+loginitems -a Caffeine
+
+# Dock management
 dockutil --remove 'Siri'
 dockutil --remove 'Launchpad'
 dockutil --remove 'Mail'
