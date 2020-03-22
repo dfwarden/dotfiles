15e8b82701e8870e76a53ce97d43068cc19aabf1
David Warden <dfwarden@gmail.com>
2020-03-22 16:27:56 -0400

    fix git scripts to work in WSL

--- git/.git_scripts/email-guess.sh
+++ git/.git_scripts/email-guess.sh
@@ -3,7 +3,9 @@
 remote=`git remote -v | awk '/\(push\)$/ {print $2}'`
 email=dfwarden@gmail.com # default
 
-if [[ $remote == *geneseo.edu* ]]; then
+echo "Remote is ${remote}, deciding which email address to use."
+
+if [[ "${remote}" == "*geneseo.edu*" ]]; then
     email=warden@geneseo.edu
     # Use Geneseo signing key
     git config user.signingKey 0x4B89C54E5455EA50
